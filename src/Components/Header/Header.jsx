2fae358514260aa1bb676302fdbeb08e44a34c45
Santiago Aguirre <santipapo98@gmail.com>
2022-10-25 03:37:29 -0500

    Adding cursor pointer to the header links

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -26,10 +26,10 @@ function Header(props) {
                 <Box sx={{flexGrow: 1}}> 
                     <Box component="img" alt="Waco Header" src={HeaderImage}/>
                 </Box>
-                <Link onClick={() => {props.topRef.current?.scrollIntoView({behavior: 'smooth'})}} sx={{mx: '30px'}} underline="none" rel="noreferrer" variant='body1'>INICIO</Link>
-                <Link onClick={() => {props.benefitsRef.current?.scrollIntoView({behavior: 'smooth'})}} sx={{mx: '30px'}} underline="none" rel="noreferrer" variant='body1'>BENEFICIOS</Link>
-                { auth_token && <Link onClick={() => {navigate('/posts')}} sx={{mx: '30px'}} underline="none" rel="noreferrer" variant='body1'>POSTS</Link>}
-                <Link onClick={() => {!auth_token ? navigate('/login') : resetToken()}} sx={{mx: '30px'}} underline="none" rel="noreferrer" variant='body1'>{auth_token ? 'Log Off' : 'Log in'}</Link>
+                <Link onClick={() => {props.topRef.current?.scrollIntoView({behavior: 'smooth'})}} sx={{mx: '30px', cursor: 'pointer'}} underline="none" rel="noreferrer" variant='body1'>INICIO</Link>
+                <Link onClick={() => {props.benefitsRef.current?.scrollIntoView({behavior: 'smooth'})}} sx={{mx: '30px', cursor: 'pointer'}} underline="none" rel="noreferrer" variant='body1'>BENEFICIOS</Link>
+                { auth_token && <Link onClick={() => {navigate('/posts')}} sx={{mx: '30px', cursor: 'pointer'}} underline="none" rel="noreferrer" variant='body1'>POSTS</Link>}
+                <Link onClick={() => {!auth_token ? navigate('/login') : resetToken()}} sx={{mx: '30px', cursor: 'pointer'}} underline="none" rel="noreferrer" variant='body1'>{auth_token ? 'Log Off' : 'Log in'}</Link>
             </Toolbar>
         </AppBar> 
     </>
